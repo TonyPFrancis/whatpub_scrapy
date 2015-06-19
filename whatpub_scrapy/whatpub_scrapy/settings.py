b82f855e3330b6a812575d34e2775160f43e8bee
TonyPFrancis <tony.612.4u@gmail.com>
2015-06-20 00:21:08 +0530

    adding srapy file

---
+++ whatpub_scrapy/whatpub_scrapy/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for whatpub_scrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'whatpub_scrapy'
+
+SPIDER_MODULES = ['whatpub_scrapy.spiders']
+NEWSPIDER_MODULE = 'whatpub_scrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'whatpub_scrapy (+http://www.yourdomain.com)'
